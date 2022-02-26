dd35442c5e8ae378eafc0a763d83b9a82b1736d8
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-02-26 15:31:05 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helloooo</Text>
+      <Text>Why!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
